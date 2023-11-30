7d131bde6999308eeddf71f11874e0d0f32abf4e
n3spR <nospher4z@gmail.com>
2023-11-30 15:46:01 +0100

    newupdate

--- jbdc/jdbc.java
+++ jbdc/jdbc.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class jdbc {
  public static void main(String[] args) throws SQLException {
- Connection maConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sle_exJDBC1","root","");
+ Connection maConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bdd_sak","root","");
  Scanner scanner = new Scanner(System.in);
 
  System.out.println("Choisir l'option");
@@ -14,29 +14,43 @@ public class jdbc {
 
  switch(operation) {
   case 1:
-   System.out.println("Nom:");
-   String nom = scanner.nextLine();
-   System.out.println("Prénom:");
-   String prenom = scanner.nextLine();
-   System.out.println("Email:");
-   String email = scanner.nextLine();
-   System.out.println("Mot de passe:");
-   String mdp = scanner.nextLine();
+  System.out.println("Nom:");
+  String nom = scanner.nextLine();
+  System.out.println("Prenom:");
+  String prenom = scanner.nextLine();
+  System.out.println("Email:");
+  String email = scanner.nextLine();
+  System.out.println("Mot de passe:");
+  String mdp = scanner.nextLine();
+  System.out.println("Votre age:");
+  int age = scanner.nextInt();
 
-   PreparedStatement maRequete = maConnection.prepareStatement("INSERT INTO user (nom,prenom,email,mdp) VALUES (?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+  PreparedStatement maRequete = maConnection.prepareStatement("INSERT INTO user (nom, prenom, email, mdp, age) VALUES (?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
    maRequete.setString(1,nom);
    maRequete.setString(2,prenom);
    maRequete.setString(3,email);
-   maRequete.setString(4,mdp);
-   maRequete.execute();
+   maRequete.setString(4,mdp); 
+   maRequete.setInt(5,age);
+  maRequete.execute();
    ResultSet userGenerer = maRequete.getGeneratedKeys();
    if (userGenerer.next()) {
     System.out.println("L'utilisateur a été inséré avec l'ID: " + userGenerer.getInt(1));
    }
    break;
- case 2:
-  System.out.println("Voici tout les users");
-   int user = scanner.nextInt();
-   PreparedStatement fetchUserQuery = maConnection.prepareStatement("SELECT * FROM user = ?"); }
+   case 2:
+  System.out.println("Ecrire l'id");
+  int userId = scanner.nextInt();
+  PreparedStatement fetchUserQuery = maConnection.prepareStatement("SELECT * FROM user WHERE id_user = ?");
+  fetchUserQuery.setInt(1, userId);
+  ResultSet resultSet = fetchUserQuery.executeQuery();
+  while (resultSet.next()){
+    System.out.println("User ID: " + resultSet.getInt("id"));
+  }
+  break;
+  case 3:
+
+ 
+
+}
 }
 }
